d4f76f9ab8abf7194a6b53bdd61427c3010a7fdc
Juan Ignacio Martínez Abadías <jim@jim-mbp.local>
2020-08-28 13:26:20 -0300

    ej1.a: calcular maximo de una matriz

--- TP1/Ej1/script.js
+++ TP1/Ej1/script.js
@@ -1,5 +1,5 @@
-var cols = 10;
-var rows = 10;
+var cols = 3;
+var rows = 3;
 
 var matrix = [];
 for (let i = 0; i < cols; i++){
@@ -10,3 +10,17 @@ for (let i = 0; i < cols; i++){
 }
 
 console.table(matrix);
+
+function max(matrix) {
+    let m = 0;
+    for (let i = 0; i < cols; i++){
+        for (let j = 0; j < rows; j++){
+            if (matrix[i][j] > m){
+                m = matrix[i][j];
+            }
+        }
+    }   
+    return m;
+}
+
+console.log("1)a) Max de matriz es: " + max(matrix));
