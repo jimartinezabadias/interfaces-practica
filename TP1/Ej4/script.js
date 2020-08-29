941366bdc9aaa26f19b6e43f0dc22d54c5f8e62e
Juan Ignacio Martínez Abadías <jim@jim-mbp.local>
2020-08-29 01:40:24 -0300

    Ej4

--- TP1/Ej4/script.js
+++ TP1/Ej4/script.js
@@ -1,17 +1,20 @@
 // Ej 4
 
 function set_pixel(image_data,x,y,r,g,b,a) {
-    let index = y * 4 + x * (image_data.width * 4);
-    let coeficiente = x * (255 / image_data.height);
-    image_data.data[index+0] = r + coeficiente;
-    image_data.data[index+1] = g + coeficiente;
-    image_data.data[index+2] = b + coeficiente;
+    let index = (x + y * image_data.width) * 4;
+    image_data.data[index+0] = r;
+    image_data.data[index+1] = g;
+    image_data.data[index+2] = b;
     image_data.data[index+3] = a;
 }
 
 function gradient(image_data,r,g,b,a) {
-    for ( let x = 0; x < image_data.height; x++){
-        for (let y = 0; y < image_data.width; y++){
+    let coeficiente = 255 / image_data.height;
+    for ( let x = 0; x < image_data.width; x++){
+        for (let y = 0; y < image_data.height; y++){
+            r = coeficiente * y;
+            g = coeficiente * y;
+            b = coeficiente * y;
             set_pixel(image_data,x,y,r,g,b,a);
         }
     }    
