515fb1483d529312d1453b24f9ccd614ce53fba5
Juan Ignacio Martínez Abadías <jim@jim-mbp.local>
2020-09-05 18:02:59 -0300

    TPE1: Brightness Filter

--- TP1/EntregaFinal/script.js
+++ TP1/EntregaFinal/script.js
@@ -229,6 +229,137 @@ function filterSepia() {
 
 }
 
+function RGBToHSL(r,g,b) {
+    // Make r, g, and b fractions of 1
+    r /= 255;
+    g /= 255;
+    b /= 255;
+  
+    // Find greatest and smallest channel values
+    let cmin = Math.min(r,g,b),
+        cmax = Math.max(r,g,b),
+        delta = cmax - cmin,
+        h = 0,
+        s = 0,
+        l = 0;
+    
+    if (delta == 0)
+        h = 0;
+    // Red is max
+    else if (cmax == r)
+        h = ((g - b) / delta) % 6;
+    // Green is max
+    else if (cmax == g)
+        h = (b - r) / delta + 2;
+    // Blue is max
+    else
+        h = (r - g) / delta + 4;
+
+    h = Math.round(h * 60);
+    
+    // Make negative hues positive behind 360°
+    if (h < 0)
+        h += 360;
+
+    // Calculate lightness
+    l = (cmax + cmin) / 2;
+
+    // Calculate saturation
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+        
+    // Multiply l and s by 100
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+
+    return {
+        'h': h,
+        's': s,
+        'l': l
+    };
+
+}
+
+function HSLToRGB(h,s,l) {
+    // Must be fractions of 1
+    s /= 100;
+    l /= 100;
+  
+    let c = (1 - Math.abs(2 * l - 1)) * s,
+        x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+        m = l - c/2,
+        r = 0,
+        g = 0,
+        b = 0;
+
+    if (0 <= h && h < 60) {
+        r = c; g = x; b = 0;
+    } else if (60 <= h && h < 120) {
+        r = x; g = c; b = 0;
+    } else if (120 <= h && h < 180) {
+        r = 0; g = c; b = x;
+    } else if (180 <= h && h < 240) {
+        r = 0; g = x; b = c;
+    } else if (240 <= h && h < 300) {
+        r = x; g = 0; b = c;
+    } else if (300 <= h && h < 360) {
+        r = c; g = 0; b = x;
+    }
+    
+    r = Math.round((r + m) * 255);
+    g = Math.round((g + m) * 255);
+    b = Math.round((b + m) * 255);
+
+    return {
+        'r': r,
+        'g': g,
+        'b': b
+    };
+
+}
+
+function filterBrightness() {
+    
+    let image_data = ctx.getImageData(0,0,canvas.width,canvas.height);
+
+    let filter_ammount = (document.querySelector("#brightness_range").value) / 100;
+    console.log(filter_ammount);
+
+    for ( let x = 0; x < image_data.width; x++){
+        for (let y = 0; y < image_data.height; y++){
+            
+            let index = ( x + y * image_data.width) * 4;
+            
+            let r = image_data.data[index];
+            let g = image_data.data[index+1];
+            let b = image_data.data[index+2];
+    
+            let hsl_pixel = RGBToHSL(r,g,b);
+            
+            if (filter_ammount <= 1) {
+                hsl_pixel.l = hsl_pixel.l * filter_ammount;
+            }
+            if (filter_ammount > 1) {
+                hsl_pixel.l = hsl_pixel.l + ( (100 - hsl_pixel.l) * ( filter_ammount - 1));
+            }
+            
+            
+            let new_rgb = HSLToRGB(hsl_pixel.h,hsl_pixel.s,hsl_pixel.l);
+
+            // update image data
+
+            r = new_rgb.r;
+            g = new_rgb.g;
+            b = new_rgb.b;
+            set_pixel(image_data,x,y,r,g,b,255);
+      
+
+        }
+    }
+
+    ctx.putImageData(image_data, 0, 0);
+
+}
+
 
 function current_mouse_position(e) {
     let bx = e.target.getBoundingClientRect();
@@ -313,3 +444,4 @@ document.addEventListener("DOMContentLoaded", initPaint);
 
 
 // no se nota que herramienta esta seleccionada
+// guardar image width and height para no aplicar los filtros a todo el canvas
