96c475de4c5dcc89ca2302d2e0552c205962636b
Juan Ignacio Martínez Abadías <jmartinezabadias@alumnos.exa.unicen.edu.ar>
2020-09-14 00:53:59 -0300

    TP2: Ej 4: verify mouse position

--- TP2/Ej4/js/main.js
+++ TP2/Ej4/js/main.js
@@ -1,32 +1,54 @@
-// VEI - TP2 - EJ1
+// VEI - TP2 - EJ4
 
 "use strict"
 
 let canvas = null;
 let context = null;
-let fileChooser = null;
-let imageUploadBtn = null;
-let currentImage = null;
+let mousePos = null;
+let fig = null;
 
 function randomInteger(min,max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function getRandomColor() {
+    
+    let r = randomInteger(0,255);
+    let g = randomInteger(0,255);
+    let b = randomInteger(0,255);
+    return `rgba(${r},${g},${b},255)`;
+
+}
+
+function getMousePos(e) {
+    let bx = e.target.getBoundingClientRect();
+
+    return {
+        x: e.clientX - bx.left,
+        y: e.clientY - bx.top
+    };
+
+}
+
+function verifyMousePos(mouseEvent) {
+    let mousePos = getMousePos(mouseEvent);
+    if (fig.pointBelongs(mousePos)){
+        let fill = getRandomColor();
+        fig.setFill(fill);
+        fig.draw();
+    }
+}
 
 function mainFunction() {
     
     canvas = document.querySelector("#myCanvas");
     context = canvas.getContext("2d");
 
-    let fig = new Circle(200,200,100,0,context);
+    fig = new Circle(200,200,100,0,context);
     fig.draw();
 
-    let point = {
-        x: 150,
-        y: 120
-    };
 
-    console.log(fig.pointBelongs(point));
+    canvas.addEventListener("click",verifyMousePos);
 }
 
 document.addEventListener("DOMContentLoaded", mainFunction);
