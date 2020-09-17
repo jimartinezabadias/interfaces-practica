6c74b23d0fa13901f2c7c81ddff1d44730b5f914
Juan Ignacio Martínez Abadías <jmartinezabadias@alumnos.exa.unicen.edu.ar>
2020-09-17 14:56:01 -0300

    TPE2/Classes: Create Chip class

---
+++ TP2/EntregaFinal/js/Chip.js
@@ -0,0 +1,11 @@
+
+const CHIP_RADIUS = 25;
+
+class Chip extends Circle {
+    
+    constructor (image,context){
+        super(50,50,CHIP_RADIUS,image,context);
+    }
+
+
+}
--- TP2/EntregaFinal/js/main.js
+++ TP2/EntregaFinal/js/main.js
@@ -10,17 +10,11 @@ async function mainFunction() {
     canvas = document.querySelector("#myCanvas");
     context = canvas.getContext("2d");
     
-    let fillImage = await Utils.loadImage('images/texture1.png');
+    let chipImage = await Utils.loadImage('images/texture1.png');
 
-    let posX = Utils.randomInteger(0,canvas.width);
-    let posY = Utils.randomInteger(0,canvas.height);
-    let width = Utils.randomInteger(20,200);
-    let height = Utils.randomInteger(20,200);
+    let chip = new Chip(chipImage,context);
     
-    // let figure = new Rectangle(posX,posY,width,height,fillImage,context);
-    let figure = new Circle(posX,posY,height,fillImage,context);
-    
-    figure.draw();
+    chip.draw();
 
 }
 
