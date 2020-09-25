127c1c73db519eb272d0c380f0e4211842e4f7ea
Juan Ignacio Martínez Abadías <jimartinezabadias@gmail.com>
2020-09-25 20:23:07 -0300

    TPE2: Add menu

--- TP2/EntregaFinal/js/main.js
+++ TP2/EntregaFinal/js/main.js
@@ -4,27 +4,42 @@
 
 let game = null;
 let canvas = null;
+let context = null;
+let menu = null;
 
-// let context = null;
-
-// let board;
-// let token;
-// let selectedToken = null;
-
-async function mainFunction() {
+async function newGame() {
     
-    canvas = document.querySelector("#myCanvas");
-    let context = canvas.getContext("2d");
-
+    menu.style.display = 'none';
+    
     game = new Game(context);
 
     if (await game.initGame()){
-       
+        
+        canvas.style.display = 'block';
+        
         game.drawBoard();
         game.drawTokens();
     
     }
+
+}
+
+function displayMenu() {
+
+    menu = document.querySelector('#game_menu');
+    menu.style.display = 'block';
     
+    let platyBtn = document.querySelector('#play_btn');
+    platyBtn.addEventListener('click',newGame);
+
+}
+
+function mainFunction() {
+    
+    canvas = document.querySelector("#myCanvas");
+    context = canvas.getContext("2d");
+    
+    displayMenu();
 
 }
 
