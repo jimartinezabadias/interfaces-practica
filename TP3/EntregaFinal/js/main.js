cae6213bb2ba56f1f8e9732b07a0e7f24d0b71f1
Juan Ignacio Martínez Abadías <jimartinezabadias@gmail.com>
2020-10-15 19:20:44 -0300

    menu items

--- TP3/EntregaFinal/js/main.js
+++ TP3/EntregaFinal/js/main.js
@@ -5,26 +5,50 @@ let paris;
 let remy;
 let ling;
 let col;
+let menu_btn;
+let menu;
 
 function handleScroll() {
     let value = window.scrollY;
     // console.log(value);
 
-    paris.style.backgroundPosition = `100% ${value * 0.12}%`;
-    ling.style.backgroundPosition = `200px ${335 - value * 0.4}px`;
-    col.style.backgroundPosition = `1070px ${430 - value * 0.4}px`;
+    // Animate Header
+    if ( value < 500 ){
+        paris.style.backgroundPosition = `100% ${value * 0.12}%`;
+        ling.style.backgroundPosition = `200px ${335 - value * 0.4}px`;
+        col.style.backgroundPosition = `1070px ${430 - value * 0.4}px`;
+    }
+        
 
 
+}
+
+function handleMenuBtn() {
+    if (menu_btn.classList.contains("active")){
+        menu_btn.classList.remove("active");
+        menu.style.visibility = "hidden";
+    } else {
+        menu_btn.classList.add("active");
+        menu.style.visibility = "visible";
+    }
 }
 
 
 
 function mainFunction() {
+    
+    // Header elements
+
     paris = document.querySelector("#paris");
     remy = document.querySelector("#header_remy");
     ling = document.querySelector("#header_linguini");
     col = document.querySelector("#header_colete");
 
+    // Menu
+    menu_btn = document.querySelector("#menu-button");
+    menu = document.querySelector("#menu-container");
+
+    menu_btn.addEventListener("click", handleMenuBtn);
     window.addEventListener("scroll", handleScroll);
     
 }
