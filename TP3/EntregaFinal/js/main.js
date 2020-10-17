ef48ffee861a9dc725e95dcdd503a8ce5bc4e5f1
Juan Ignacio Martínez Abadías <jimartinezabadias@gmail.com>
2020-10-17 01:27:16 -0300

    TPE3: init card 3d

--- TP3/EntregaFinal/js/main.js
+++ TP3/EntregaFinal/js/main.js
@@ -16,6 +16,11 @@ let knife;
 let fork;
 let menu_items;
 
+// cards
+let ling_card;
+let cX;
+let cY;
+
 
 function handleScroll() {
     let value = window.scrollY;
@@ -30,20 +35,23 @@ function handleScroll() {
 
 }
 
-function handleMenuBtn() {
+async function handleMenuBtn() {
     if (menu_btn.classList.contains("visible")){
         menu_btn.classList.remove("visible");
-        // menu.style.visibility = "hidden";
         
         mantel.classList.remove("mantel_active");
         plate.classList.remove("plate_active");
         knife.classList.remove("knife_active");
         fork.classList.remove("fork_active");
         menu_items.classList.remove("menu_items_active");
+        
+        await new Promise(r => setTimeout(r, 500));
+
+        menu.style.visibility = "hidden";
 
     } else {
         menu_btn.classList.add("visible");
-        // menu.style.visibility = "visible";
+        menu.style.visibility = "visible";
 
         mantel.classList.add("mantel_active");
         plate.classList.add("plate_active");
@@ -53,6 +61,28 @@ function handleMenuBtn() {
     }
 }
 
+function mouseMove(e) {
+    // console.log(e.layerX);
+    // e.relatedTarget	Returns the element related to the element that triggered the mouse event
+
+    let mouseX = e.layerX;
+    let mouseY = e.layerY;
+
+    let dX = mouseX - cX;
+    let dY = mouseY - cY;
+    
+    let transform = "";
+
+    transform += `rotateX(${ -dY * 0.1 }deg) rotateY(${ dX * 0.1 }deg)`; 
+
+    ling_card.style.transform = transform;
+
+}
+
+function mouseLeave(e) {
+    console.log("out");
+    ling_card.style.transition = "transform 0.3 ease-in";
+}
 
 
 function mainFunction() {
@@ -72,9 +102,16 @@ function mainFunction() {
     fork = document.querySelector("#menu-fork");
     menu_items = document.querySelector("#menu-items");
 
+    // Cards
+    ling_card = document.querySelector("#ling_card");
+    cX = ling_card.clientWidth / 2 ;
+    cY = ling_card.clientHeight / 2 ;
+
     // Events
     menu_btn.addEventListener("click", handleMenuBtn);
     window.addEventListener("scroll", handleScroll);
+    ling_card.addEventListener("mousemove", mouseMove);
+    ling_card.addEventListener("mouseout", mouseLeave);
     
 }
 
